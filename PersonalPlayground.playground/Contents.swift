32acae06b56cf0d9212727153e8b7cd055d9eb1a
Aerisolstice <46410704+Aerisolstice@users.noreply.github.com>
2019-01-17 12:58:25 -1000

    Practicing with Bools

---
+++ PersonalPlayground.playground/Contents.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+var str = "Hello, playground"
+print(str)
+
+var P1a = 1
+var S1Pa = "P1a = 1"
+print(S1Pa)
+
+var P1b = 4
+var S1Pb = "P1b = 4"
+print(S1Pb)
+
+var compare1 = P1a > P1b
+var state1 = "P1a > P1b"
+print(state1)
+
+var Thus1 = "This statement is"
+print(Thus1)
+print(compare1)
+
+var state2a = "Using the same values, evaluate"
+print(state2a)
+var state2 = "P1a < P1b"
+print(state2)
+
+var compare2 = P1a < P1b
+print(Thus1)
+print(compare2)
+
+
+
+
